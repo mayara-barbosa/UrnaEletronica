f321035a6d5954b29125cb6f95745b49ba39f975
Mayara <maay.bs@hotmail.com>
2016-09-17 18:25:23 -0300

    cliente enviando para server

--- UrnaEletronicaClient/src/urnaeletronicaclient/UrnaEletronicaClient.java
+++ UrnaEletronicaClient/src/urnaeletronicaclient/UrnaEletronicaClient.java
@@ -5,6 +5,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 /**
@@ -13,26 +14,27 @@ import java.util.Scanner;
  */
 public class UrnaEletronicaClient {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        Socket urnaCliente;
-        urnaCliente  = new Socket("127.0.0.1", 9999);
-        
-        int escolha;
+    public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
+        // conecta socket passando ip e porta
+        Socket client;
+        client = new Socket("127.0.0.1", 9999);
+        int numero,numeroCandidato;
         Scanner sc;
         sc = new Scanner(System.in);
         
-        System.out.println("Digite 1 para votar:");
-        escolha = sc.nextInt();
+        System.out.println("Digite 1 para votar: ");
+        numero = sc.nextInt();
+        //System.out.println("Numero candidato: ");
+        //numeroCandidato = sc.nextInt();
         
         OutputStream os;
-        os = urnaCliente.getOutputStream();
+        os = client.getOutputStream();
         DataOutputStream dos;
         dos = new DataOutputStream(os);
-        
         for(;;){
-            dos.writeInt(escolha);
-            Thread.sleep(1000);
-        }
+			dos.writeInt(numero);                        
+			Thread.sleep(1000);
+		}
         
                 
         
