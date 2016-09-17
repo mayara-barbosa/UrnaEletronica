bff3d8f39d163f6ca17459cce9a5c8da8a8a64d1
Mayara <maay.bs@hotmail.com>
2016-09-17 15:22:41 -0300

    urna client criada

---
+++ UrnaEletronicaClient/src/urnaeletronicaclient/UrnaEletronicaClient.java
@@ -0,0 +1,13 @@
+package urnaeletronicaclient;
+
+/**
+ *
+ * @author Mayara
+ */
+public class UrnaEletronicaClient {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
