68be0672f5b9af3c8422b468d40e2639bb9b73f9
mayara-barbosa <>
2016-09-23 15:58:59 -0300

    classe candidato e criar lista de candidatos no server

---
+++ UrnaEletronicaServer/src/urnaeletronicaserver/Candidato.java
@@ -0,0 +1,30 @@
+package urnaeletronicaserver;
+
+/**
+ *
+ * @author mayara.barbosa
+ */
+public class Candidato {
+    
+    private int codigo;
+    private int votos;
+
+      
+    public int getCodigo(){
+        return codigo;
+    }
+    
+    public int getVotos(){
+        return votos;
+    }
+
+    void setCodigo(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setVotos(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
--- UrnaEletronicaServer/src/urnaeletronicaserver/UrnaEletronicaServer.java
+++ UrnaEletronicaServer/src/urnaeletronicaserver/UrnaEletronicaServer.java
@@ -3,6 +3,8 @@ package urnaeletronicaserver;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -11,8 +13,27 @@ import java.net.Socket;
 public class UrnaEletronicaServer {
 
     public static void main(String[] args) throws IOException {
+        List<Candidato> votos = new ArrayList<Candidato>();
         ServerSocket urnaServer;
         urnaServer = new ServerSocket(9999);
+        
+        Candidato c = new Candidato();
+        c.setCodigo(0);
+        c.setVotos(0);
+        votos.add(c);
+        Candidato c2 = new Candidato();
+        c2.setCodigo(1);
+        c2.setVotos(0);
+        votos.add(c2);
+        Candidato c3 = new Candidato();
+        c3.setCodigo(2);
+        c3.setVotos(0);
+        votos.add(c3);
+        Candidato c4 = new Candidato();
+        c4.setCodigo(3);
+        c4.setVotos(0);
+        votos.add(c4);   
+        
         
         for(;;){
             Socket cliente;
